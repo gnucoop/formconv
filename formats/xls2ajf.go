b31ea9e74f0be68ad440d48d66868c0a7fee04f9
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-04-25 11:27:11 +0100

    recursive group creation implemented. Assignment of ids still needs fixing

--- formats/xls2ajf.go
+++ formats/xls2ajf.go
@@ -7,95 +7,28 @@ import (
 )
 
 func Xls2ajf(xls *XlsForm) (*AjfForm, error) {
-	survey, err := checkGroups(xls.Survey)
-	if err != nil {
-		return nil, err
-	}
 	var ajf AjfForm
 	var choicesMap map[string][]Choice
 	ajf.ChoicesOrigins, choicesMap = buildChoicesOrigins(xls.Choices)
 
-	curNodes := &ajf.Slides
-	nodesStack := []*[]Node{curNodes}
-	for _, row := range survey {
-		switch {
-		case row.Type == beginGroup:
-			nodeType := NtGroup
-			if len(nodesStack) == 1 {
-				nodeType = NtSlide
-			}
-			*curNodes = append(*curNodes, Node{
-				Name:  row.Name,
-				Label: row.Label,
-				Type:  nodeType,
-				Nodes: make([]Node, 0), // This becomes the new curNodes:
-			})
-			curNodes = &(*curNodes)[len(*curNodes)-1].Nodes
-			nodesStack = append(nodesStack, curNodes)
-		case row.Type == endGroup:
-			nodesStack = nodesStack[0 : len(nodesStack)-1]
-			curNodes = nodesStack[len(nodesStack)-1]
-		case isSupportedField(row.Type):
-			*curNodes = append(*curNodes, Node{
-				Name:      row.Name,
-				Label:     row.Label,
-				Type:      NtField,
-				FieldType: fieldTypeFrom(row.Type),
-			})
-			curField := &(*curNodes)[len(*curNodes)-1]
-			if *curField.FieldType == FtSingleChoice || *curField.FieldType == FtMultipleChoice {
-				choiceName := row.Type[strings.Index(row.Type, " ")+1:]
-				if _, present := choicesMap[choiceName]; !present {
-					return nil, fmt.Errorf("Undefined choice %s", choiceName)
-				}
-				curField.ChoicesOriginRef = choiceName
-			} else if *curField.FieldType == FtNote {
-				curField.HTML = row.Label
-			}
-			if row.Required == "yes" {
-				curField.Validation = &FieldValidation{NotEmpty: true}
-			}
-		case isUnsupportedField(row.Type):
-			return nil, fmt.Errorf("Field type %q is not supported", row.Type)
-		case row.Type == beginRepeat || row.Type == endRepeat:
-			return nil, fmt.Errorf("Repeats are not supported")
-		default:
-			return nil, fmt.Errorf("Invalid type %q in survey", row.Type)
-		}
+	survey, err := preprocessGroups(xls.Survey)
+	if err != nil {
+		return nil, err
+	}
+	global, err := buildGroup(survey, 0)
+	if err != nil {
+		return nil, err
+	}
+	err = checkChoicesRef(&global, choicesMap)
+	if err != nil {
+		return nil, err
+	}
+	ajf.Slides = global.Nodes
+	for i := range ajf.Slides {
+		ajf.Slides[i].Type = NtSlide
 	}
 	assignIds(&ajf)
 	return &ajf, nil
-}
-
-var notBalancedErr = errors.New("Groups are not balanced")
-
-func checkGroups(survey []SurveyRow) ([]SurveyRow, error) {
-	groupDepth := 0
-	ungroupedItems := false
-	for _, row := range survey {
-		switch row.Type {
-		case beginGroup:
-			groupDepth++
-		case endGroup:
-			groupDepth--
-			if groupDepth < 0 {
-				return nil, notBalancedErr
-			}
-		default:
-			if groupDepth == 0 {
-				ungroupedItems = true
-			}
-		}
-	}
-	if groupDepth != 0 {
-		return nil, notBalancedErr
-	}
-	if ungroupedItems || len(survey) == 0 {
-		// Wrap everything into a big group/slide.
-		survey = append([]SurveyRow{{Type: beginGroup, Name: "form", Label: "Form"}}, survey...)
-		survey = append(survey, SurveyRow{Type: endGroup})
-	}
-	return survey, nil
 }
 
 func buildChoicesOrigins(rows []ChoicesRow) ([]ChoicesOrigin, map[string][]Choice) {
@@ -116,6 +49,152 @@ func buildChoicesOrigins(rows []ChoicesRow) ([]ChoicesOrigin, map[string][]Choic
 		})
 	}
 	return co, choicesMap
+}
+
+var notBalancedErr = errors.New("Groups are not balanced")
+
+func preprocessGroups(survey []SurveyRow) ([]SurveyRow, error) {
+	groupDepth := 0
+	ungroupedItems := false
+	for _, row := range survey {
+		switch row.Type {
+		case beginGroup:
+			groupDepth++
+		case endGroup:
+			groupDepth--
+			if groupDepth < 0 {
+				return nil, notBalancedErr
+			}
+		default:
+			if groupDepth == 0 {
+				ungroupedItems = true
+			}
+		}
+	}
+	if groupDepth != 0 {
+		return nil, notBalancedErr
+	}
+	if ungroupedItems {
+		// Wrap everything into a slide.
+		survey = append([]SurveyRow{{Type: beginGroup, Name: "form", Label: "Form"}}, survey...)
+		survey = append(survey, SurveyRow{Type: endGroup})
+	}
+	// Wrap everything into a global phony group,
+	// it allows building the form with a single call to buildGroup.
+	survey = append([]SurveyRow{{Type: beginGroup, Name: "global", Label: "Global"}}, survey...)
+	survey = append(survey, SurveyRow{Type: endGroup})
+	return survey, nil
+}
+
+func buildGroup(survey []SurveyRow, previous int) (Node, error) {
+	// survey[0] is a beginGroup row
+	group := Node{
+		Id:       previous + 1,
+		Previous: previous,
+		Name:     survey[0].Name,
+		Label:    survey[0].Label,
+		Type:     NtGroup,
+		Nodes:    make([]Node, 0),
+	}
+	for i := 1; i < len(survey); i++ {
+		row := survey[i]
+		switch {
+		case row.Type == beginGroup:
+			end := groupEnd(survey, i)
+			child, err := buildGroup(survey[i:end], previous+1)
+			if err != nil {
+				return Node{}, err
+			}
+			group.Nodes = append(group.Nodes, child)
+			i = end - 1
+		case row.Type == endGroup:
+			if i != len(survey)-1 {
+				panic("unexpected end of group")
+			}
+		case isSupportedField(row.Type):
+			field := buildField(&row, previous+1)
+			group.Nodes = append(group.Nodes, field)
+		case isUnsupportedField(row.Type):
+			return Node{}, fmt.Errorf("Field type %q is not supported", row.Type)
+		case row.Type == beginRepeat || row.Type == endRepeat:
+			return Node{}, fmt.Errorf("Repeats are not supported")
+		default:
+			return Node{}, fmt.Errorf("Invalid type %q in survey", row.Type)
+		}
+	}
+	return group, nil
+}
+
+func groupEnd(survey []SurveyRow, group int) int {
+	groupDepth := 1
+	for i := group + 1; i < len(survey); i++ {
+		switch survey[i].Type {
+		case beginGroup:
+			groupDepth++
+		case endGroup:
+			groupDepth--
+			if groupDepth == 0 {
+				return i + 1
+			}
+		}
+	}
+	panic("group end not found")
+}
+
+func buildField(row *SurveyRow, previous int) Node {
+	field := Node{
+		Id:       previous + 1,
+		Previous: previous,
+		Name:     row.Name,
+		Label:    row.Label,
+		Type:     NtField,
+	}
+	switch {
+	case row.Type == "decimal":
+		field.FieldType = &FtNumber
+	case row.Type == "text":
+		field.FieldType = &FtString
+	case row.Type == "select_one yes_no":
+		field.FieldType = &FtBoolean
+	case isSelectOne(row.Type):
+		field.FieldType = &FtSingleChoice
+		field.ChoicesOriginRef = row.Type[strings.Index(row.Type, " ")+1:]
+	case isSelectMultiple(row.Type):
+		field.FieldType = &FtMultipleChoice
+		field.ChoicesOriginRef = row.Type[strings.Index(row.Type, " ")+1:]
+	case row.Type == "note":
+		field.FieldType = &FtNote
+		field.HTML = row.Label
+	case row.Type == "date":
+		field.FieldType = &FtDate
+	case row.Type == "time":
+		field.FieldType = &FtTime
+	case row.Type == "calculate":
+		field.FieldType = &FtFormula
+	case isUnsupportedField(row.Type):
+		panic("unsupported row type: " + row.Type)
+	default:
+		panic("unrecognized row type: " + row.Type)
+	}
+	if row.Required == "yes" {
+		field.Validation = &FieldValidation{NotEmpty: true}
+	}
+	return field
+}
+
+func checkChoicesRef(node *Node, choicesMap map[string][]Choice) error {
+	if node.Type == NtField && (*node.FieldType == FtSingleChoice || *node.FieldType == FtMultipleChoice) {
+		if _, ok := choicesMap[node.ChoicesOriginRef]; !ok {
+			return fmt.Errorf("Invalid single or multiple choice %q", node.ChoicesOriginRef)
+		}
+	}
+	for i := range node.Nodes {
+		err := checkChoicesRef(&node.Nodes[i], choicesMap)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 const (
@@ -149,33 +228,6 @@ var unsupportedField = map[string]bool{
 
 func isUnsupportedField(typ string) bool { return unsupportedField[typ] || isRank(typ) }
 func isRank(typ string) bool             { return strings.HasPrefix(typ, "rank ") }
-
-func fieldTypeFrom(typ string) *FieldType {
-	switch {
-	case typ == "decimal":
-		return &FtNumber
-	case typ == "text":
-		return &FtString
-	case typ == "select_one yes_no":
-		return &FtBoolean
-	case isSelectOne(typ):
-		return &FtSingleChoice
-	case isSelectMultiple(typ):
-		return &FtMultipleChoice
-	case typ == "note":
-		return &FtNote
-	case typ == "date":
-		return &FtDate
-	case typ == "time":
-		return &FtTime
-	case typ == "calculate":
-		return &FtFormula
-	case isUnsupportedField(typ):
-		panic("unsupported type: " + typ)
-	default:
-		panic("unrecognized type: " + typ)
-	}
-}
 
 func assignIds(ajf *AjfForm) {
 	for i := range ajf.Slides {
