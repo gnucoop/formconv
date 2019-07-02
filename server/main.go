08549a551467123f2c8b3bcdf23b1b87b534e0ab
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2019-07-02 15:45:44 +0200

    server: implementing cors

--- server/main.go
+++ server/main.go
@@ -17,19 +17,36 @@ func main() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir("server/static")))
-	http.HandleFunc("/result.json", convert)
+	http.HandleFunc("/result.json", handleConversion)
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func convert(w http.ResponseWriter, r *http.Request) {
-	enableCors(w)
-	if r.Method == http.MethodOptions {
-		w.WriteHeader(http.StatusOK)
-		return
+func handleConversion(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodOptions:
+		optionsConversion(w, r)
+	case http.MethodGet:
+		getConversion(w, r)
+	case http.MethodPost:
+		postConversion(w, r)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Unsupported method %s", r.Method)
 	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+}
 
+func optionsConversion(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+}
+
+func getConversion(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s", "You should POST an excel file here;",
+		`<a href="/index.html">go to the index page</a>`)
+}
+
+func postConversion(w http.ResponseWriter, r *http.Request) {
 	f, head, err := r.FormFile("excelFile")
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -50,12 +67,9 @@ func convert(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, err)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	err = formats.EncAjf(w, ajf)
 	if err != nil {
 		log.Printf("Error writing json response: %s", err)
 	}
-}
-
-func enableCors(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 }
